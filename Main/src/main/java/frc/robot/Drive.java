38364c493abdb82df9ca681862696a90a2558605
zoring147 <zoring1470@gmail.com>
2022-10-11 19:40:33 -0400

    newer version

--- Main/src/main/java/frc/robot/Drive.java
+++ Main/src/main/java/frc/robot/Drive.java
@@ -1,77 +1,54 @@
 package frc.robot;
 
-import com.playingwithfusion.*;
-import com.playingwithfusion.CANVenom.BrakeCoastMode;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 
 public class Drive extends RobotSubsystems
 {
-  public static CANVenom leftFront = new CANVenom(2);
   //public static CANVenom leftBack = new CANVenom(4);
-  public static CANVenom rightFront = new CANVenom(1);
-  public static CANVenom rightBack = new CANVenom(3);
-  public static TalonSRX leftBack = new TalonSRX(3);
+  public static WPI_TalonSRX rightFront = new WPI_TalonSRX(4);
+  public static WPI_TalonSRX rightBack = new WPI_TalonSRX(2);
+  public static WPI_TalonSRX leftBack = new WPI_TalonSRX(3);
+  public static WPI_TalonSRX leftFront = new WPI_TalonSRX(1);
 
+  //private DifferentialDrive m_myRobot;
   //public static MotorControllerGroup leftMotors = new MotorControllerGroup(leftFront, leftBack);
   //public static MotorControllerGroup rightMotors = new MotorControllerGroup(rightFront, rightBack);
 
   public void robotInit() {
-    leftFront.setB(0);
-    leftFront.setKF(0.184);
-    leftFront.setKP(0.05);
-    leftFront.setKI(0.0);
-    leftFront.setKD(0.0);
-
-    rightFront.setB(0);
-    rightFront.setKF(0.184);
-    rightFront.setKP(0.05);
-    rightFront.setKI(0.0);
-    rightFront.setKD(0.0);
-
+    //check how wired
     leftFront.setInverted(false);
     leftBack.setInverted(false);
     rightFront.setInverted(true);
     rightBack.setInverted(true);
 
     rightBack.follow(rightFront);
+    leftBack.follow(leftFront);
     //rightBack.follow(rightFront); replaced with Talon on CIM for RB
 
-    leftFront.setBrakeCoastMode(BrakeCoastMode.Coast);
-    //leftBack.setBrakeCoastMode(BrakeCoastMode.Coast);
+    leftFront.setNeutralMode(NeutralMode.Coast);
     leftBack.setNeutralMode(NeutralMode.Coast);
-    rightFront.setBrakeCoastMode(BrakeCoastMode.Coast);
-    rightBack.setBrakeCoastMode(BrakeCoastMode.Coast);
+    rightFront.setNeutralMode(NeutralMode.Coast);
+    rightBack.setNeutralMode(NeutralMode.Coast);
+
+    //m_myRobot = new DifferentialDrive(leftFront, rightFront);
   }
   public void robotPeriodic() {
   }
 
   public void teleopInit() {
-    venomEncoderReset();
   }
 
   public void teleopPeriodic() {
-    exponentialDrive(deadband(Robot.controllerOne.getLeftY()), deadband(Robot.controllerOne.getRightY()));
-    talonSRXFollowVenom(leftFront, leftBack, 6);
-  }
-  public static void venomEncoderReset() {
-    //leftBack.resetPosition();
-    leftFront.resetPosition();
-    rightBack.resetPosition();
-    rightFront.resetPosition();
+    nonlinearDrive(Robot.controllerOne.getLeftY(), Robot.controllerOne.getRightY());
   }
   
   public void autonomousInit() {
-    venomEncoderReset();
   }
 
   public void autonomousPeriodic() {}
 
   public void testInit() {
-    venomEncoderReset();
   }
 
   /*public void testPeriodic() {
@@ -83,13 +60,9 @@ public class Drive extends RobotSubsystems
     }
   }*/
 
-  public static void talonSRXFollowVenom(CANVenom leader, TalonSRX follower, double maxMotorVolt) {
-    if(leader.getOutputVoltage() > 10)  {follower.configVoltageCompSaturation(maxMotorVolt);}
-    else  {follower.configVoltageCompSaturation(maxMotorVolt*2);}
-    follower.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, leader.getOutputVoltage()/maxMotorVolt);
-  }
+ 
 
-  /*public void nonlinearDrive(double left, double right) {
+  public void nonlinearDrive(double left, double right) {
     double deadband = 0.11;
 
     if(Math.abs(left) > deadband){
@@ -98,23 +71,14 @@ public class Drive extends RobotSubsystems
     if(Math.abs(right) > deadband){
       rightFront.set(-1.0*Math.signum(right)*right*right);
     } else {rightFront.set(0.0);}
-    if(Math.abs(left) > deadband){
+    /*if(Math.abs(left) > deadband){
       leftBack.set(-1.0*Math.signum(left)*left*left);
     } else {leftBack.set(0.0);}
     if(Math.abs(left) > deadband){
       rightBack.set(-1.0*Math.signum(right)*right*right);
-    } else {rightBack.set(0.0);}
-  }*/
-
-  public double deadband(double input) {
-    double output;
-    if(Math.abs(input) < 0.15) {
-      output = 0;
-    } else {
-      output = input;
-    }
-    return output;
+    } else {rightBack.set(0.0);}*/
   }
+
 
   /*public void linearDrive(double left, double right)  {
     leftFront.set(-left);
@@ -122,11 +86,4 @@ public class Drive extends RobotSubsystems
     leftBack.set(-left);
     rightBack.set(ControlMode.Velocity, -right);
   }*/
-  
-  public void exponentialDrive(double left, double right) {
-    leftFront.set(-(0.2*Math.tan(1.4*left)));
-    //leftBack.set(TalonSRXControlMode.PercentOutput, -(0.2*Math.tan(1.4*left)));
-    rightFront.set(-(0.2*Math.tan(1.4*right)));
-    rightBack.set(-(0.2*Math.tan(1.4*right)));
-  }
 }
